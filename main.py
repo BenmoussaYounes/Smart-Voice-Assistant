d60872299ea6358545738560c5500f5a9f5a78a4
younes <younesnanos@gmail.com>
2023-03-18 19:48:30 +0100

    1803

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import win32api
 from neuralintents import GenericAssistant
 import speech_recognition
 import pyttsx3 as tts
+
+
 from mapping import mapping
 
 recognizer = speech_recognition.Recognizer()
 
 speaker = tts.init()
-speaker.setProperty('rate', 150)
+speaker.setProperty('rate', 120)
+
 
 assistant = GenericAssistant('intents.json', intent_methods=mapping)
 assistant.train_model()
@@ -15,12 +18,10 @@ assistant.train_model()
 while True:
     try:
         with speech_recognition.Microphone as Mic:
-         recognizer.adjust_for_ambient_noise(Mic, duration=0.2)
-         audio  = recognizer.listen(Mic)
-
-         message = recognizer.recognize_google(audio)
-         message = message.lower()
-
+          recognizer.adjust_for_ambient_noise(Mic, duration=0.2)
+          audio  = recognizer.listen(Mic)
+          message = recognizer.recognize_google(audio)
+          message = message.lower()
         assistant.request(message)
 
     except speech_recognition.UnknownValueError:
--- mapping.py
+++ mapping.py
@@ -3,6 +3,9 @@ import win32api
 import pyttsx3 as tts
 import sys
 import speech_recognition
+import json
+
+
 
 recognizer = speech_recognition.Recognizer()
 
@@ -12,7 +15,6 @@ speaker.setProperty('rate', 150)
 
 def create_note():
     global recognizer
-
     speaker.say('What you want to add to your list ?')
     speaker.runAndWait()
 
@@ -24,12 +26,15 @@ def create_note():
                 audio = recognizer.listen(mic)
                 note = recognizer.recognize_google(audio)
                 note = note.lower()
+
                 speaker.say("Chose a file name ?")
                 speaker.runAndWait()
+
                 recognizer.adjust_for_ambient_noise(mic, duration=0.2)
-                audio =  recognizer.listen(mic)
+                audio = recognizer.listen(mic)
                 filename = recognizer.recognize_google(audio)
                 filename = filename.lower()
+
             with open(filename, 'w') as f:
                 f.write(note)
                 done = True
@@ -59,7 +64,6 @@ def add_todo():
 
                 speaker.say('Chose a file name')
                 speaker.runAndWait()
-
                 recognizer.adjust_for_ambient_noise(Mic, duration=0.2)
                 audio = recognizer.listen(Mic)
                 file_name  = recognizer.recognize_google(Mic)
@@ -77,10 +81,16 @@ def add_todo():
            speaker.runAndWait()
 
 
+todolist = ["Clean the room","study for school","go shooping"]
+def show_todos():
+    speaker.say("The item on your list are ")
+    for item in todolist:
+        speaker.say(item)
+    speaker.runAndWait()
 
 
 def hello():
-    speaker.say("Hello what i can do for you ?")
+    speaker.say("Hello what you need help with ")
     speaker.runAndWait()
 
 def exit():
@@ -89,7 +99,11 @@ def exit():
     sys.exit(0)
 
 
-mapping = {'greeting': hello(),
-           'create_note':create_note(),
-           'exit':exit()
-           }
+mapping = {
+    "create_note": create_note(),
+    "add_todo": add_todo(),
+    "exit": exit(),
+    "show_todos": show_todos(),
+    "greeting": hello(),
+}
+
